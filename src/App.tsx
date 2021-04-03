eb5407e39b9824d6a8e1fad026f95ce33c98aef4
snamiki1212 <snamiki1212@gmail.com>
2021-04-03 13:46:32 -0700

    Update logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  useQuery,
+  useMutation,
+  gql,
+} from "@apollo/client";
+// import {} from '@apollo/client/link'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          items: {
+            read(cached) {
+              return cached ?? [];
+            },
+          },
+        },
+      },
+    },
+  }),
+});
+
+const Provider: React.FC = ({ children }) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <div>ok</div>
+      <Items />
+      <Handler />
+    </Provider>
   );
 }
+
+const GetItems = gql`
+  query GetItems {
+    items @client
+  }
+`;
+
+const Noop = gql`
+  mutation Noop {
+    items @client
+  }
+`;
+
+const Handler = () => {
+  const [noop] = useMutation(Noop, {
+    update(cache) {
+      const prevData = cache.readQuery({
+        query: GetItems,
+      });
+      const prev = (prevData as any)?.items;
+
+      const result = cache.writeQuery({
+        query: GetItems,
+        data: { items: [...prev, "new"] },
+      });
+
+      console.log("UPDATE:", result);
+    },
+  });
+
+  const handleAdd = async () => {
+    const result = await noop();
+    console.log("ok", result);
+  };
+  return (
+    <div>
+      <button onClick={handleAdd}>+</button>
+    </div>
+  );
+};
+
+const Items = () => {
+  const { data, loading, error } = useQuery(GetItems);
+  if (loading) return <div>loading...</div>;
+  if (error) return <div>error: {error.message}</div>;
+  if (!data) return <div>no data</div>;
+  const items = data?.items;
+  console.log("ITEMS, ", items);
+  if (!Array.isArray(items)) return <div>loading</div>;
+
+  return (
+    <div>
+      <h3>This is Items</h3>
+      {items?.map((item) => (
+        <div key={item}>{item}</div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
